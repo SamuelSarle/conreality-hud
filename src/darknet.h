b44879589b937fd8a2ac3876e91a9c0c00fec443
Samuel Sarle <samuelsarle@gmail.com>
2018-05-12 15:24:18 +0300

    Updated the darknet files according to Yolo-v3.

--- src/darknet.h
+++ src/darknet.h
@@ -9,9 +9,9 @@
 
 #include "yolo_v2_class.hpp"
 
-void drawBoxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> object_names);
-void showConsoleResult(std::vector<bbox_t> const result_vec, std::vector<std::string> const object_names);
-cv::Scalar objectIdToColor(int obj_id);
+void drawBoxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names, int current_det_fps = -1, int current_cap_fps = -1);
+cv::Scalar objIdToColor(int obj_id);
+void showConsoleResult(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names);
 std::vector<std::string> objectNamesFromFile(std::string const filename);
 
 #endif //DARKNET_H
